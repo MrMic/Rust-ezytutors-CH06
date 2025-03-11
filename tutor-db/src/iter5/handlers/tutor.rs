9ef4b07b42044e940806f6dbb94253aeac1bfa85
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2025-03-11 16:33:57 +0100

    feat(UX): Handler functions for tutor routes

--- tutor-db/src/iter5/handlers/tutor.rs
+++ tutor-db/src/iter5/handlers/tutor.rs
@@ -1,1 +1,59 @@
+// tutor-db/src/iter5/handlers/tutor.rs
+use crate::dbaccess::tutor::*;
+use crate::models::tutor::NewTutor;
+use crate::{errors::EzyTutorError, state::AppState};
+use actix_web::{
+    web::{self},
+    HttpResponse,
+};
 
+// ______________________________________________________________________
+pub async fn get_all_tutors(app_state: web::Data<AppState>) -> Result<HttpResponse, EzyTutorError> {
+    get_all_tutors_db(&app_state.db)
+        .await
+        .map(|tutors| HttpResponse::Ok().json(tutors))
+}
+
+// ______________________________________________________________________
+pub async fn get_tutor_details(
+    app_state: web::Data<AppState>,
+    path: web::Path<i32>,
+) -> Result<HttpResponse, EzyTutorError> {
+    let tutor_id = path.into_inner();
+    get_tutor_details_db(&app_state.db, tutor_id)
+        .await
+        .map(|tutor| HttpResponse::Ok().json(tutor))
+}
+
+// ______________________________________________________________________
+pub async fn post_new_tutor(
+    new_tutor: web::Json<NewTutor>,
+    app_state: web::Data<AppState>,
+) -> Result<HttpResponse, EzyTutorError> {
+    post_new_tutor_db(&app_state.db, new_tutor::from(new_tutor))
+        .await
+        .map(|tutor| HttpResponse::Ok().json(tutor))
+}
+
+// ______________________________________________________________________
+pub async fn update_tutor_details(
+    app_state: web::Data<AppState>,
+    path: web::Path<i32>,
+    update_tutor: web::Json<NewTutor>,
+) -> Result<HttpResponse, EzyTutorError> {
+    let tutor_id = path.into_inner();
+    update_tutor_details_db(&app_state.db, tutor_id, update_tutor::from(update_tutor))
+        .await
+        .map(|tutor| HttpResponse::Ok().json(tutor))
+}
+
+// ______________________________________________________________________
+pub async fn delete_tutor(
+    app_state: web::Data<AppState>,
+    path: web::Path<i32>,
+) -> Result<HttpResponse, EzyTutorError> {
+    let tutor_id = path.into_inner();
+    delete_tutor_db(&app_state.db, tutor_id)
+        .await
+        .map(|tutor| HttpResponse::Ok().json(tutor))
+}
